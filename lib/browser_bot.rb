6bdadcb92f6e18978cb7ea98523a4bb504a6f360
jiikko <n905i.1214@gmail.com>
2016-04-24 15:53:18 +0900

    fix

--- lib/browser_bot.rb
+++ lib/browser_bot.rb
@@ -5,34 +5,27 @@ require 'pry'
 module BrowsserBot
   CONDFIG_FILE = 'config.yml'
 
-  def self.run(params)
+  def self.run
     extend Capybara::DSL
+    browser_init
     # ここにかく
+    #
+    #
+    visit 'http://google.com'
+    sleep 10
   end
 
   private
 
-  def self.browser_init(headless)
-    if headless
-      require 'capybara/poltergeist'
-      # https://github.com/teampoltergeist/poltergeist
-      Capybara.javascript_driver = :poltergeist
-      Capybara.current_driver = :poltergeist
-      Capybara.register_driver :poltergeist do |app|
-        Capybara::Poltergeist::Driver.new(app, {
-          js_errors: false,
-        })
-      end
-    else
-      browser_name = :chrome
-      Capybara.run_server = false
-      Capybara.register_driver(browser_name) do |app|
-        Capybara::Selenium::Driver.new(app, {
-          browser: browser_name, # run firefox if blank or :firefox
-        })
-      end
-      Capybara.current_driver = browser_name
+  def self.browser_init
+    browser_name = :chrome
+    Capybara.run_server = false
+    Capybara.register_driver(browser_name) do |app|
+      Capybara::Selenium::Driver.new(app, {
+        browser: browser_name, # run firefox if blank or :firefox
+      })
     end
-    Capybara.reset_sessions!
+    Capybara.current_driver = browser_name
   end
+  Capybara.reset_sessions!
 end
